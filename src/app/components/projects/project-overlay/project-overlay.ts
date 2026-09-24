b250970027fab28f6bdcec33a91704ee3847b43a
Sven Apel <svap1201@gmail.com>
2026-09-24 16:10:05 +0200

    chore: generate shared, section and page components

---
+++ src/app/components/projects/project-overlay/project-overlay.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-project-overlay',
+  styleUrl: './project-overlay.scss',
+  templateUrl: './project-overlay.html',
+})
+export class ProjectOverlay {}
---
+++ src/app/components/references/references.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-references',
+  styleUrl: './references.scss',
+  templateUrl: './references.html',
+})
+export class References {}
