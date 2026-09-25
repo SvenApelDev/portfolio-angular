ba1054eb044a95a4a1752dc9f528c48ec7f3152f
Sven Apel <svap1201@gmail.com>
2026-09-25 14:59:23 +0200

    feat: add routes for home, legal notice and privacy policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { LegalNotice } from './pages/legal-notice/legal-notice';
+import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home, title: 'Sven Apel | Frontend Developer' },
+  {
+    path: 'legal-notice',
+    component: LegalNotice,
+    title: 'Legal Notice | Sven Apel | Frontend Developer',
+  },
+  {
+    path: 'privacy-policy',
+    component: PrivacyPolicy,
+    title: 'Privacy Policy | Sven Apel | Frontend Developer',
+  },
+  { path: '**', redirectTo: '' },
+];
